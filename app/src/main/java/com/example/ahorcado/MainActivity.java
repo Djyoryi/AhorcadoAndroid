4610078141f86035a5e3b46c4f221a2fa43a3d0d
Djyoryi <fecq86@gmail.com>
2019-04-25 19:26:30 +0200

    sin bloquear los botones

--- app/src/main/java/com/example/ahorcado/MainActivity.java
+++ app/src/main/java/com/example/ahorcado/MainActivity.java
@@ -6,69 +6,76 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Switch;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     String palabraOculta = "CETYS";
     int numeroDeFallos = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        if (savedInstanceState == null){
+        if (savedInstanceState == null) {
             //
             getSupportFragmentManager().beginTransaction().add(R.id.ventanaJuego, new VentanaAhorcado()).commit();
         }
 
 
-        }
-    public void botonPulsado(View vista){
-        Button boton = (Button) findViewById(vista.getId());
-        boton.setVisibility(View.INVISIBLE);
+    }
 
-        chequeaLetra(boton.getText().toString());
+    public void botonPulsado(View vista) {
+        Button boton = (Button) findViewById(vista.getId());
+        boton.setClickable(false);
+
+
+        chequeaLetra(boton);
 
         //NotificationCompat.Builder = new NotificationCompat.Builder(this,"0");
 
     }
 
-    private void chequeaLetra(String letra) {
+    private void chequeaLetra(Button boton) {
         ImageView imagenAhorcadoIV = findViewById(R.id.imagenAhorcado);
         String nombreImagen = imagenAhorcadoIV.toString();
         TextView textoOcultoTV = findViewById(R.id.palabraConGuiones);
         String palabraConGuiones = textoOcultoTV.getText().toString();
+        String letra = boton.getText().toString();
+        boton.setBackgroundColor(getResources().getColor(R.color.white));
         boolean acierto = false;
-        boolean partidaTerminada = false;
-            for (int i=0; i<palabraOculta.length(); i++){
-                if (palabraOculta.charAt(i) == letra.charAt(0)){
-                    palabraConGuiones = palabraConGuiones.substring(0, 2*i) + letra
-                            + palabraConGuiones.substring(2*i+1);
-                    acierto=true;
-                    }
-                }
-            textoOcultoTV.setText(palabraConGuiones);
-        if (!acierto){
+        for (int i = 0; i < palabraOculta.length(); i++) {
+            if (palabraOculta.charAt(i) == letra.charAt(0)) {
+                palabraConGuiones = palabraConGuiones.substring(0, 2 * i) + letra
+                        + palabraConGuiones.substring(2 * i + 1);
+                acierto = true;
+                boton.setTextColor(getResources().getColor(R.color.green));
+            }
+        }
+        textoOcultoTV.setText(palabraConGuiones);
+        if (!acierto) {
+            boton.setTextColor(getResources().getColor(R.color.red));
             numeroDeFallos++;
             pintaFallos(numeroDeFallos);
-            if (numeroDeFallos >= 7){
-                partidaTerminada = true;
 
-            }
         }
         //si el if se cumple es porque ya no hay guiones en el display
-        if (!palabraConGuiones.contains("_")){
+        if (!palabraConGuiones.contains("_")) {
             pintaFallos(-1);
-            partidaTerminada = true;
+            bloqueaBotones();
 
         }
-            }
+    }
+
     private void pintaFallos(int numeroFallos) {
         ImageView imagenAhorcadoIV = findViewById(R.id.imagenAhorcado);
-        switch (numeroFallos){
+        switch (numeroFallos) {
             case 0:
                 imagenAhorcadoIV.setImageResource(R.drawable.ahorcado_1);
                 break;
@@ -90,11 +97,35 @@ public class MainActivity extends AppCompatActivity {
             case -1:
                 imagenAhorcadoIV.setImageResource(R.drawable.acertaste_todo);
                 break;
-            default :
+            default:
                 imagenAhorcadoIV.setImageResource(R.drawable.looser);
                 break;
 
         }
     }
+
+    private void bloqueaBotones() {
+        LinearLayout container = findViewById(R.id.contenedor);
+        ArrayList<LinearLayout> arrayListLL = new ArrayList<>();
+        for (int i = 0; i < container.getChildCount(); i++) {
+            if(container.getChildAt(i)instanceof LinearLayout) {
+                arrayListLL.add((LinearLayout)container.getChildAt(i));
+            }
+        }
+        bloqueaBotonesDelArrayList(arrayListLL);
+
+
     }
+
+    private void bloqueaBotonesDelArrayList(ArrayList<LinearLayout> arrayListLL) {
+        for (LinearLayout l:arrayListLL) {
+            for (int i = 0; i < l.getChildCount(); i++) {
+                if (l.getChildAt(i)instanceof Button) {
+                    Button b = (Button) l.getChildAt(i);
+                    b.setClickable(false);
+                }
+            }
+        }
+    }
+}
 
